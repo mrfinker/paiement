ddf23968bba5906563a363d829b2ad22c385676a
mrfinker <caalebs@gmail.com>
2023-09-04 11:33:18 +0000

    4 sept. 2023, 12:33

--- dashboard.php
+++ dashboard.php
@@ -1133,21 +1133,3 @@ require_once('./models/authentication_code.php');
                     </div>
                 </div>
                 <!-- content @e -->
-                <!-- footer @s -->
-                <div class="nk-footer bg-white">
-                    <div class="container-fluid">
-                        <div class="nk-footer-wrap">
-                            <div class="nk-footer-copyright">
-                                &copy; 2023
-                                <a href="https://linked-solution" target="_blank">linked-solution</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- footer @e -->
-            </div>
-            <!-- wrap @e -->
-        </div>
-        <!-- app-root @e -->
-
-        
--- views/include/footer.php
+++ views/include/footer.php
@@ -1,5 +1,23 @@
 <!-- JavaScript -->
-<script src="./public/assets/js/bundle.js?ver=3.2.0"></script>
+<!-- footer @s -->
+<div class="nk-footer bg-white">
+                    <div class="container-fluid">
+                        <div class="nk-footer-wrap">
+                            <div class="nk-footer-copyright">
+                                &copy; 2023
+                                <a href="https://linked-solution" target="_blank">linked-solution</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer @e -->
+            </div>
+            <!-- wrap @e -->
+        </div>
+        <!-- app-root @e -->
+
+        
+        <script src="./public/assets/js/bundle.js?ver=3.2.0"></script>
         <script src="./public/assets/js/scripts.js?ver=3.2.0"></script>
     </body>
 
