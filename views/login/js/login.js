16f99ce2403178b8cc7a9349f1bf5ae1e7e6c924
mrfinker <caalebs@gmail.com>
2023-09-04 13:52:16 +0000

    4 sept. 2023, 14:52

---
+++ views/login/js/login.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    const baseUrl = "http://localhost/PaimentLinked/"
+        $(document).on("submit", "#registerFormUser", function(e) {
+            e.preventDefault();
+            let name = $("#name").val()
+            let email = $("#email").val()
+            let username = $("#username").val()
+            let phone = $("#phone").val()
+            let address = $("#address").val()
+            let password = $("#password").val()
+            let confirmPassword = $("#confirmPassword").val()
+            $.ajax({
+                url: `${baseUrl}login/handleRegister`,
+                type: "POST",
+                data: {
+                    name,
+                    username,
+                    email,
+                    phone,
+                    address,
+                    password,
+                    confirmPassword,
+                    action: "jddiuanjkanciuwenfas,mcn;sdiojd"
+                },
+                success: function(res) {
+                    if (res === "success") {
+                        window.location = `${baseUrl}chat`
+                    } else {
+                        alert(res)
+                    }
+                }
+            })
+        })
+    
+        $(document).on("submit", "#loginFormUser", function(e) {
+            e.preventDefault();
+            let email = $("#email").val()
+            let password = $("#password").val()
+            $.ajax({
+                url: `${baseUrl}login/handleLogin`,
+                type: "POST",
+                data: {
+                    email,
+                    password,
+                    action: "jddiuanjkanciuSFDSFAEEEADS;sdiojd"
+                },
+                success: function(res) {
+                    if (res === "success") {
+                        window.location = `${baseUrl}chat`
+                    } else {
+                        alert(res)
+                    }
+                }
+            })
+        })
+    })
