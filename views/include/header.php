639021f19a50a8f44733cba67e4252eb149968cf
mrfinker <caalebs@gmail.com>
2023-09-04 11:36:36 +0000

    4 sept. 2023, 12:36

--- views/include/header.php
+++ views/include/header.php
@@ -1,4 +1,41 @@
-<div class="nk-header is-light">
+<?php 
+require_once('config.php');
+require_once('./models/authentication_code.php');
+?>
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+    <head>
+        <base href="../">
+        <meta charset="utf-8">
+        <meta name="author" content="Softnio">
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta
+            name="description"
+            content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
+        <!-- Fav Icon -->
+        <link rel="shortcut icon" href="./public/images/favicon.png">
+        <!-- Page Title -->
+        <title>Blank - Layout | DashLite Admin Template</title>
+        <!-- StyleSheets -->
+        <link rel="stylesheet" href="./public/assets/css/dashlite.css?ver=3.2.0">
+        <link
+            id="skin-default"
+            rel="stylesheet"
+            href="./public/assets/css/theme.css?ver=3.2.0">
+    </head>
+
+    <body class="nk-body bg-lighter ">
+        <div class="nk-app-root">
+            <?php 
+        // include('./message.php') 
+        ?>
+            <!-- wrap @s -->
+            <div class="nk-wrap ">
+                <!-- main header @s -->
+                <div class="nk-header is-light">
                     <div class="container-fluid">
                         <div class="nk-header-wrap">
                             <div class="nk-menu-trigger me-sm-2 d-lg-none">
