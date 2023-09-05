56ed1725da79f28968ef040e4a0d75d8222b9b18
mrfinker <caalebs@gmail.com>
2023-09-05 13:26:12 +0000

    5 sept. 2023, 14:26

--- views/dashboard/js/dashboard.js
+++ views/dashboard/js/dashboard.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
     const baseUrl = "http://paiement.mr:81/";
 
+    $.ajax({
+        url: `${baseUrl}dashboard`, 
+        type: "GET",
+        success: function (data) {
+            console.log(data);
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
     
+    $("#logoutButton").on("click", function () {
+        $.ajax({
+            url: `${baseUrl}logout`, 
+            type: "POST",
+            success: function () {
+                window.location = `${baseUrl}login`;
+            },
+            error: function (error) {
+                console.error(error);
+            }
+        });
+    });
 });
-  
