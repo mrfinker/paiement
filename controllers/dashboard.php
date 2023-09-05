97f623214a3cfe3f0148adba6fcc250e1c5e9e07
mrfinker <caalebs@gmail.com>
2023-09-05 12:15:04 +0000

    5 sept. 2023, 13:15

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-class Dashbord extends Controller{
+class Dashboard extends Controller{
     function __construct(){
         parent::__construct();
         $this->view->js = array("dashboard/js/dashboard.js");
