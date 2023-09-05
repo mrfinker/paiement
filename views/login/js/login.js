bd3f4f32a097da2c08b9bbf6b63af8e16481c571
mrfinker <caalebs@gmail.com>
2023-09-05 11:55:48 +0000

    5 sept. 2023, 12:55

--- views/login/js/login.js
+++ views/login/js/login.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
       },
       success: function (res) {
         if (res === "success") {
-          window.location = `${baseUrl}chat`;
+          window.location = `${baseUrl}dashboard`;
         } else {
           alert(res);
         }
