1df19cc6c023f1ee6602437d16392733b1c62837
mrfinker <caalebs@gmail.com>
2023-09-05 13:17:00 +0000

    5 sept. 2023, 14:17

--- views/login/js/login.js
+++ views/login/js/login.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-  const baseUrl = "http://paiement.mr:81/";
+    const baseUrl = "http://paiement.mr:81/"
 
-  $(document).on("submit", "#loginFormUser", function (e) {
-    e.preventDefault();
-    let email = $("#email").val();
-    let password = $("#password").val();
-    $.ajax({
-      url: `${baseUrl}login/handleLogin`,
-      type: "POST",
-      data: {
-        email,
-        password,
-        action: "jddiuanjkanciuSFDSFAEEEADS;sdiojd",
-      },
-      success: function (res) {
-        if (res === "success") {
-          window.location = `${baseUrl}dashboard`;
-        } else {
-          console.log(res);
-        }
-      },
-    });
-  });
-});
+        $(document).on("submit", "#loginFormUser", function(e) {
+            e.preventDefault();
+            let email = $("#email").val()
+            let password = $("#password").val()
+            $.ajax({
+                url: `${baseUrl}login/handleLogin`,
+                type: "POST",
+                data: {
+                    email,
+                    password,
+                    action: "jddiuanjkanciuSFDSFAEEEADS;sdiojd"
+                },
+                success: function(res) {
+                    if (res === "success") {
+                        window.location = `${baseUrl}chat`
+                    } else {
+                        alert(res)
+                    }
+                }
+            })
+        })
+})
