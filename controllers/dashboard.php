161c135ba260d17a06b9cb3440fdf464ff6f60d1
mrfinker <caalebs@gmail.com>
2023-09-05 12:01:04 +0000

    5 sept. 2023, 13:01

---
+++ controllers/dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+class Dashbord extends Controller{
+    function __construct(){
+        parent::__construct();
+    }
+
+    function index(){
+        $this->view->render('dashbord/index', true);
+    }
+}
+
+?>
