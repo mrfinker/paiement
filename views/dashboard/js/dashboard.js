e94b727084e089b6ec30846cf3ed0d7257245e45
mrfinker <caalebs@gmail.com>
2023-09-05 16:33:33 +0000

    5 sept. 2023, 17:33

--- views/dashboard/js/dashboard.js
+++ views/dashboard/js/dashboard.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     const baseUrl = "http://paiement.mr:81/";
 
-    $("#logout_btn").on("click", function () {
+    $("#logout_btn").on("click", function (e) {
+        e.preventDefault();
         $.ajax({
-            url: `${baseUrl}login/logout`, 
+            url: `${baseUrl}login/logout`,
             type: "POST",
             success: function () {
+                
                 window.location = `${baseUrl}login`;
             },
             error: function (error) {
