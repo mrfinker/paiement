492dc488aea14e2b6e4e982769afd61323fa715f
mrfinker <caalebs@gmail.com>
2023-09-06 13:56:05 +0000

    6 sept. 2023, 14:56

--- controllers/profileuser.php
+++ controllers/profileuser.php
@@ -5,7 +5,7 @@ class Profileuser extends Controller
     public function __construct()
     {
         parent::__construct();
-        $this->view->js = array("dashboard/js/profiles.js");
+        $this->view->js = array("dashboard/js/dashboard.js");
     }
 
     public function profileuser()
