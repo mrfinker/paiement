b32b584c7ad8dd52997f802c001c0afc4ea22070
mrfinker <caalebs@gmail.com>
2023-09-06 01:46:32 +0000

    6 sept. 2023, 02:46

--- models/register_model.php
+++ models/register_model.php
@@ -9,6 +9,10 @@ class Register_model extends Model{
     function getUserbyEmail(string $email){
         return $this->db->select("SELECT * FROM users WHERE email = :email LIMIT 1", array("email" => $email));
     }
+    
+    function getUserbyUsername(string $username){
+        return $this->db->select("SELECT * FROM users WHERE username = :username LIMIT 1", array("userame" => $userame));
+    }
 
     function saveUser(array $data) {
         return $this->db->insert("users", $data);
