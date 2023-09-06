ac6a02313f0718f0bc6a739e189ac147bc320926
mrfinker <caalebs@gmail.com>
2023-09-06 11:53:56 +0000

    6 sept. 2023, 12:53

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -10,7 +10,7 @@ class Dashboard extends Controller
 
     public function index()
     {
-        $this->view->render('dashboard/index', true);
+        $this->view->render('dashboard/superadmin', true);
     }
     public function admin()
     {
