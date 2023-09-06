0e1bc57754223b3a683dd58e609d81eed253f24e
mrfinker <caalebs@gmail.com>
2023-09-06 10:28:15 +0000

    6 sept. 2023, 11:28

--- views/login/index.php
+++ views/login/index.php
@@ -79,7 +79,7 @@
                     <div class="nk-footer nk-auth-footer-full">
                         <div class="container wide-lg">
                             <div class="row g-3">
-                                <div class="col-lg-6 order-lg-last">
+                            <div class="col-lg-6 order-lg-last">
                                     <ul class="nav nav-sm justify-content-center justify-content-lg-end">
                                         <li class="nav-item">
                                             <a class="nav-link" href="<?= URL ?>termes">Termes &amp; Condition</a>
@@ -91,6 +91,11 @@
                                             <a class="nav-link" href="linked-solution.com">Aide</a>
                                         </li>
                                     </ul>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="nk-block-content text-center text-lg-left">
+                                        <p class="text-soft">© 2023 Linked-solution. Tout droit reservé.</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
