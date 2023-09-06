15d7850120984aa02e321f13dea23cab2f3d642b
mrfinker <caalebs@gmail.com>
2023-09-06 13:03:11 +0000

    6 sept. 2023, 14:03

--- controllers/profileuser.php
+++ controllers/profileuser.php
@@ -1,28 +1,13 @@
 <?php
 
+class Profileuser extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->js = array("dashboard/js/profiles.js");
+    }
 
-Session::init();
-
-if (isset($_SESSION['users']) && isset($_SESSION['userType'])) {
-    $user = Session::get('users');
-    $userType = Session::get('userType');
-} else {
-    header("Location: " . LOGIN);
-    exit;
+    
+    
 }
-
-if ($userType['name'] === "superadmin") {
-    include('superadmin.php');
-} elseif ($userType['name'] === "admin") {
-    include('admin.php');
-} elseif ($userType['name'] === "staff") {
-    include('staff.php');
-} elseif ($userType['name'] === "company") {
-    include('company.php');
-} else {
-    header('Location: ' . ERROR);
-    exit;
-}
-
-
-?>
---
+++ models/profileuser_model.php
@@ -0,0 +1,34 @@
+<?php
+
+class Register_model extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
+
+Session::init();
+    
+    if (isset($_SESSION['users']) && isset($_SESSION['userType'])) {
+        $user = Session::get('users');
+        $userType = Session::get('userType');
+    } else {
+        header("Location: " . LOGIN);
+        exit;
+    }
+    
+    if ($userType['name'] === "superadmin") {
+        include('superadmin.php');
+    } elseif ($userType['name'] === "admin") {
+        include('admin.php');
+    } elseif ($userType['name'] === "staff") {
+        include('staff.php');
+    } elseif ($userType['name'] === "company") {
+        include('company.php');
+    } else {
+        header('Location: ' . ERROR);
+        exit;
+    }
