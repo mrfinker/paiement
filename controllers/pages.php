08ae4451a4cdc0fdb30b7f40d55c307fa373f507
mrfinker <caalebs@gmail.com>
2023-09-06 10:39:07 +0000

    6 sept. 2023, 11:39

---
+++ controllers/pages.php
@@ -0,0 +1,19 @@
+<?php
+
+class Dashboard extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->js = array("pages/js/pages.js");
+    }
+
+    public function termes()
+    {
+        $this->view->render('pages/termes', true);
+    }
+    public function faq()
+    {
+        $this->view->render('pages/faq', true);
+    }
+}
