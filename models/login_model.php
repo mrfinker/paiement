9a4bf6a18a8fbdb36f0abcccdbc10354ff14da32
mrfinker <caalebs@gmail.com>
2023-09-06 03:17:40 +0000

    6 sept. 2023, 04:17

--- models/login_model.php
+++ models/login_model.php
@@ -13,14 +13,4 @@ class Login_model extends Model{
     function saveUser(array $data) {
         return $this->db->insert("users", $data);
     }
-
-    function getUserByEmailOrUsernameWithRole(string $identifier) {
-        $sql = "SELECT u.*, ut.user_type AS role_type 
-        FROM users u
-        LEFT JOIN user_types ut ON u.user_type_id = ut.id_type
-        WHERE u.email = :identifier OR u.username = :identifier
-        LIMIT 1";
-        return $this->db->select($sql, array("identifier" => $identifier));
-    }
-    
 }
