ace25391b0cbb74b954b2ae1967929fa2ce0fa40
mrfinker <caalebs@gmail.com>
2023-09-06 04:10:45 +0000

    6 sept. 2023, 05:10

--- controllers/login.php
+++ controllers/login.php
@@ -45,58 +45,6 @@ class Login extends Controller {
             $this->view->render('login/index', true);
         }
     }
-    
-
-    function handleRegister()
-    {
-        if (isset($_POST["action"]) && $_POST['action'] == "jddiuanjkanciuwenfas,mcn;sdiojd") {
-            $nom = htmlspecialchars($_POST["nom"]);
-            $username = htmlspecialchars($_POST["username"]);
-            $email = htmlspecialchars($_POST["email"]);
-            $phone = htmlspecialchars($_POST["phone"]);
-            $address = htmlspecialchars($_POST["address"]);
-            $password = htmlspecialchars($_POST["password"]);
-            $confirmPassword = htmlspecialchars($_POST["confirmPassword"]);
-            if (
-                !empty($nom) &&
-                !empty($username) &&
-                !empty($email) &&
-                !empty($phone) &&
-                !empty($address) &&
-                !empty($password) &&
-                !empty($confirmPassword)
-            ) {
-                if ($password === $confirmPassword) {
-                    $getUserByEmail = $this->model->getUserByEmail($email);
-                    if (empty($getUserByEmail)) {
-                        $save = $this->model->saveUser(array(
-                            "nom" => $nom,
-                            "username" => $username,
-                            "email" => $email,
-                            "phone" => $phone,
-                            "address" => $address,
-                            "pwd" => $password
-                        ));
-                        if ($save) {
-                            $getUserByEmail = $this->model->getUserByEmail($email);
-                            Session::set("users", $getUserByEmail[0]);
-                            echo "success";
-                        } else {
-                            echo "Une erreur de traitement";
-                        }
-                    } else {
-                        echo "L'adresse email existe déjà";
-                    }
-                } else {
-                    echo "Les deux mots de passe doivent être identiques";
-                }
-            } else {
-                echo "Tous les champs sont obligatoires";
-            }
-        } else {
-            echo "Pas d'autorisation";
-        }
-    }
 
     function handleLogin()
     {
@@ -129,4 +77,3 @@ class Login extends Controller {
         Session::destroy();
     }
 }
-
