1d2719929aff67586bf2d1bb1aa490789f4f5349
mrfinker <caalebs@gmail.com>
2023-09-06 03:33:02 +0000

    6 sept. 2023, 04:33

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -6,35 +6,6 @@ class Dashboard extends Controller{
         $this->view->js = array("dashboard/js/dashboard.js");
     }
 
-    function dashboard()
-    {
-        if (Session::get("users")) {
-            $userRole = Session::get("users")['role_type'];
-    
-            switch ($userRole) {
-                case 'superadmin':
-                    $this->view->render('dashboard/superadmin', true);
-                    break;
-                case 'admin':
-                    $this->view->render('dashboard/admin', true);
-                    break;
-                case 'company':
-                    $this->view->render('dashboard/company', true);
-                    break;
-                case 'staff':
-                    $this->view->render('dashboard/staff', true);
-                    break;
-                default:
-                    // Si le rôle n'est pas défini ou inconnu, redirigez l'utilisateur vers une page d'erreur ou le tableau de bord par défaut.
-                    $this->view->render('error/index', true);
-                    break;
-            }
-        } else {
-            // Si l'utilisateur n'est pas connecté, redirigez-le vers la page de connexion.
-            $this->view->render('login/index', true);
-        }
-    }
-
 }
 
 ?>
