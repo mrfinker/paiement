9da2ecb9e0ac4b4fc39fb9bc49d20ed7f9dfdf88
mrfinker <caalebs@gmail.com>
2023-09-06 13:09:36 +0000

    6 sept. 2023, 14:09

--- views/dashboard/userprofile.php
+++ views/dashboard/userprofile.php
@@ -33,12 +33,12 @@ Session::init();
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="A powerful and conceptual apps base dashboard template that especially build for developers and programmers.">
     <!-- Fav Icon  -->
-    <link rel="shortcut icon" href="./images/favicon.png">
+    <link rel="shortcut icon" href="<?= URL ?>public<?= URL ?>public/images/favicon.png">
     <!-- Page Title  -->
     <title>Profile | DashLite Admin Template</title>
     <!-- StyleSheets  -->
-    <link rel="stylesheet" href="./assets/css/dashlite.css?ver=3.2.0">
-    <link id="skin-default" rel="stylesheet" href="./assets/css/theme.css?ver=3.2.0">
+    <link rel="stylesheet" href="<?= URL ?>public/assets/css/dashlite.css?ver=3.2.0">
+    <link id="skin-default" rel="stylesheet" href="<?= URL ?>public/assets/css/theme.css?ver=3.2.0">
 </head>
 
 <body class="nk-body bg-lighter ">
@@ -54,16 +54,16 @@ Session::init();
                         </div>
                         <div class="nk-header-brand">
                             <a href="html/index.html" class="logo-link">
-                                <img class="logo-light logo-img" src="./images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
-                                <img class="logo-dark logo-img" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                                <img class="logo-light logo-img" src="<?= URL ?>public/images/logo.png" srcset="<?= URL ?>public/images/logo2x.png 2x" alt="logo">
+                                <img class="logo-dark logo-img" src="<?= URL ?>public/images/logo-dark.png" srcset="<?= URL ?>public/images/logo-dark2x.png 2x" alt="logo-dark">
                             </a>
                         </div><!-- .nk-header-brand -->
                         <div class="nk-header-menu ms-auto" data-content="headerNav">
                             <div class="nk-header-mobile">
                                 <div class="nk-header-brand">
                                     <a href="html/index.html" class="logo-link">
-                                        <img class="logo-light logo-img" src="./images/logo.png" srcset="./images/logo2x.png 2x" alt="logo">
-                                        <img class="logo-dark logo-img" src="./images/logo-dark.png" srcset="./images/logo-dark2x.png 2x" alt="logo-dark">
+                                        <img class="logo-light logo-img" src="<?= URL ?>public/images/logo.png" srcset="<?= URL ?>public/images/logo2x.png 2x" alt="logo">
+                                        <img class="logo-dark logo-img" src="<?= URL ?>public/images/logo-dark.png" srcset="<?= URL ?>public/images/logo-dark2x.png 2x" alt="logo-dark">
                                     </a>
                                 </div>
                                 <div class="nk-menu-trigger me-n2">
@@ -635,7 +635,7 @@ Session::init();
                                                 <li class="chat-item">
                                                     <a class="chat-link" href="html/apps-chats.html">
                                                         <div class="chat-media user-avatar">
-                                                            <img src="./images/avatar/b-sm.jpg" alt="">
+                                                            <img src="<?= URL ?>public/images/avatar/b-sm.jpg" alt="">
                                                         </div>
                                                         <div class="chat-info">
                                                             <div class="chat-from">
@@ -652,7 +652,7 @@ Session::init();
                                                     <a class="chat-link" href="html/apps-chats.html">
                                                         <div class="chat-media user-avatar user-avatar-multiple">
                                                             <div class="user-avatar">
-                                                                <img src="./images/avatar/c-sm.jpg" alt="">
+                                                                <img src="<?= URL ?>public/images/avatar/c-sm.jpg" alt="">
                                                             </div>
                                                             <div class="user-avatar">
                                                                 <span>AB</span>
@@ -675,7 +675,7 @@ Session::init();
                                                 <li class="chat-item">
                                                     <a class="chat-link" href="html/apps-chats.html">
                                                         <div class="chat-media user-avatar">
-                                                            <img src="./images/avatar/a-sm.jpg" alt="">
+                                                            <img src="<?= URL ?>public/images/avatar/a-sm.jpg" alt="">
                                                             <span class="status dot dot-lg dot-success"></span>
                                                         </div>
                                                         <div class="chat-info">
@@ -718,32 +718,32 @@ Session::init();
                                 <li class="dropdown language-dropdown d-none d-sm-block me-n1">
                                     <a href="#" class="dropdown-toggle nk-quick-nav-icon" data-bs-toggle="dropdown">
                                         <div class="quick-icon border border-light">
-                                            <img class="icon" src="./images/flags/english-sq.png" alt="">
+                                            <img class="icon" src="<?= URL ?>public/images/flags/english-sq.png" alt="">
                                         </div>
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-s1">
                                         <ul class="language-list">
                                             <li>
                                                 <a href="#" class="language-item">
-                                                    <img src="./images/flags/english.png" alt="" class="language-flag">
+                                                    <img src="<?= URL ?>public/images/flags/english.png" alt="" class="language-flag">
                                                     <span class="language-name">English</span>
                                                 </a>
                                             </li>
                                             <li>
                                                 <a href="#" class="language-item">
-                                                    <img src="./images/flags/spanish.png" alt="" class="language-flag">
+                                                    <img src="<?= URL ?>public/images/flags/spanish.png" alt="" class="language-flag">
                                                     <span class="language-name">Español</span>
                                                 </a>
                                             </li>
                                             <li>
                                                 <a href="#" class="language-item">
-                                                    <img src="./images/flags/french.png" alt="" class="language-flag">
+                                                    <img src="<?= URL ?>public/images/flags/french.png" alt="" class="language-flag">
                                                     <span class="language-name">Français</span>
                                                 </a>
                                             </li>
                                             <li>
                                                 <a href="#" class="language-item">
-                                                    <img src="./images/flags/turkey.png" alt="" class="language-flag">
+                                                    <img src="<?= URL ?>public/images/flags/turkey.png" alt="" class="language-flag">
                                                     <span class="language-name">Türkçe</span>
                                                 </a>
                                             </li>
@@ -1007,109 +1007,109 @@ Session::init();
                         <ul class="country-list text-center gy-2">
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/arg.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/arg.png" alt="" class="country-flag">
                                     <span class="country-name">Argentina</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/aus.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/aus.png" alt="" class="country-flag">
                                     <span class="country-name">Australia</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/bangladesh.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/bangladesh.png" alt="" class="country-flag">
                                     <span class="country-name">Bangladesh</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/canada.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/canada.png" alt="" class="country-flag">
                                     <span class="country-name">Canada <small>(English)</small></span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/china.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/china.png" alt="" class="country-flag">
                                     <span class="country-name">Centrafricaine</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/china.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/china.png" alt="" class="country-flag">
                                     <span class="country-name">China</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/french.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/french.png" alt="" class="country-flag">
                                     <span class="country-name">France</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/germany.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/germany.png" alt="" class="country-flag">
                                     <span class="country-name">Germany</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/iran.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/iran.png" alt="" class="country-flag">
                                     <span class="country-name">Iran</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/italy.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/italy.png" alt="" class="country-flag">
                                     <span class="country-name">Italy</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/mexico.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/mexico.png" alt="" class="country-flag">
                                     <span class="country-name">México</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/philipine.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/philipine.png" alt="" class="country-flag">
                                     <span class="country-name">Philippines</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/portugal.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/portugal.png" alt="" class="country-flag">
                                     <span class="country-name">Portugal</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/s-africa.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/s-africa.png" alt="" class="country-flag">
                                     <span class="country-name">South Africa</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/spanish.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/spanish.png" alt="" class="country-flag">
                                     <span class="country-name">Spain</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/switzerland.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/switzerland.png" alt="" class="country-flag">
                                     <span class="country-name">Switzerland</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/uk.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/uk.png" alt="" class="country-flag">
                                     <span class="country-name">United Kingdom</span>
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="country-item">
-                                    <img src="./images/flags/english.png" alt="" class="country-flag">
+                                    <img src="<?= URL ?>public/images/flags/english.png" alt="" class="country-flag">
                                     <span class="country-name">United State</span>
                                 </a>
                             </li>
@@ -1230,8 +1230,8 @@ Session::init();
         </div><!-- .modal-dialog -->
     </div><!-- .modal -->
     <!-- JavaScript -->
-    <script src="./assets/js/bundle.js?ver=3.2.0"></script>
-    <script src="./assets/js/scripts.js?ver=3.2.0"></script>
+    <script src="<?= URL ?>public/assets/js/bundle.js?ver=3.2.0"></script>
+    <script src="<?= URL ?>public/assets/js/scripts.js?ver=3.2.0"></script>
 </body>
 
 </html>
