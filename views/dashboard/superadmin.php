408a532c5028e61bda57c9fc83b7d6f85034e944
mrfinker <caalebs@gmail.com>
2023-09-06 10:00:19 +0000

    6 sept. 2023, 11:00

--- views/dashboard/superadmin.php
+++ views/dashboard/superadmin.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-if (isset($_SESSION['users']) && isset($_SESSION['userType'])!=='surperadmin') {
+if (isset($_SESSION['users']) && isset($_SESSION['TypeName'])!=='surperadmin') {
     $user = $_SESSION['users'];
     $userType = $_SESSION['userType'];
 } else {
