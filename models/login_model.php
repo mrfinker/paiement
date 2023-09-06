3aa77fa0777af011f1418ee9fa6eadcef5a26e90
mrfinker <caalebs@gmail.com>
2023-09-06 04:14:37 +0000

    6 sept. 2023, 05:14

--- models/login_model.php
+++ models/login_model.php
@@ -18,12 +18,24 @@ class Login_model extends Model{
         return $this->db->insert("users", $data);
     }
 
-    function getUserByEmailOrUsernameWithRole(string $identifier) {
-        return $this->db->select("SELECT u.*, ut.user_type AS role_type 
-        FROM users u
-        LEFT JOIN user_types ut ON u.user_type_id = ut.id_type
-        WHERE u.email = :identifier OR u.username = :identifier
-        LIMIT 1");
-    }
+    function getUserByEmailOrUsernameWithRole(string $identifier)
+{
+    // Utilisez une requête préparée pour éviter les injections SQL.
+    $query = "SELECT u.*, ut.user_type AS role_type 
+              FROM users u
+              LEFT JOIN user_types ut ON u.user_type_id = ut.id_type
+              WHERE u.email = :identifier OR u.username = :identifier
+              LIMIT 1";
+    
+    // Utilisez une requête préparée pour sécuriser la requête SQL.
+    $params = array(':identifier' => $identifier);
+    
+    // Utilisez la méthode de requête préparée de votre base de données pour exécuter la requête.
+    $result = $this->db->select($query, $params);
+    
+    // Retournez le résultat de la requête.
+    return $result;
+}
+
     
 }
