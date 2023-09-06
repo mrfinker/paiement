19e8f972e68f8c27e36409c98b910ef9844191ba
mrfinker <caalebs@gmail.com>
2023-09-06 04:25:24 +0000

    6 sept. 2023, 05:25

--- models/login_model.php
+++ models/login_model.php
@@ -19,7 +19,7 @@ class Login_model extends Model{
 {
     return $this->db->select("SELECT u.*, ut.user_type AS role_type 
     FROM users u
-    LEFT JOIN user_types ut ON u.user_type_id = ut.id_type
+    LEFT JOIN user_type ut ON u.user_type_id = ut.id_type
     WHERE u.email = :identifier OR u.username = :identifier
     LIMIT 1", array("identifier" => $identifier));
 }
