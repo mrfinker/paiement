71bf5c846333c976da2809976433f7e07586988f
mrfinker <caalebs@gmail.com>
2023-09-07 09:22:37 +0000

    7 sept. 2023, 10:22

--- views/login/index.php
+++ views/login/index.php
@@ -43,12 +43,10 @@
                                     <img
                                         class="logo-light logo-img logo-img-lg"
                                         src="public/images/logo.png"
-                                        srcset="public/images/logo2x.png 2x"
                                         alt="logo">
                                     <img
                                         class="logo-dark logo-img logo-img-lg"
                                         src="public/images/logo-dark.png"
-                                        srcset="public/images/logo-dark2x.png 2x"
                                         alt="logo-dark">
                                 </a>
                             </div>
