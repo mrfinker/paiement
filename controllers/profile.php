5d7be4e476893588040785b7107f950adc98dfb6
mrfinker <caalebs@gmail.com>
2023-09-07 08:52:10 +0000

    7 sept. 2023, 09:52

--- controllers/profile.php
+++ controllers/profile.php
@@ -7,20 +7,5 @@ class Profile extends Controller
         parent::__construct();
         $this->view->js = array("dashboard/js/dashboard.js");
     }
-
-    public function profile($role)
-    {
-        if($role === 'superadmin'){
-            $this->view->render('dashboard/profile', true);
-        }elseif($role === 'admin'){
-            $this->view->render('dashboard/profile', true);
-        }elseif($role === 'staff'){
-            $this->view->render('dashboard/profile', true);
-        }elseif($role === 'company'){
-            $this->view->render('dashboard/profile', true);
-        }else{
-            $this->view->render('error/index', true);
-        }
-    }
     
 }
